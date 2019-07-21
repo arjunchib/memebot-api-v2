a0c8160b0e086c1e9eb144557c6bc777e26bb443
Arjun Chib <arjunpatrickchib@gmail.com>
2019-07-21 17:05:09 -0400

    Block mutauions if not from memebot ip

--- src/resolvers/removeTagFromMeme.js
+++ src/resolvers/removeTagFromMeme.js
@@ -1,4 +1,7 @@
-module.exports = async function({ name, tag }, { db }) {
+module.exports = async function({ name, tag }, { ip, db }) {
+  if (ip !== process.env.MEMEBOT_IP && process.env.NODE_ENV !== 'development') {
+    throw new Error('You are not authenticated to mutation data!')
+  }
   const memes = db.collection('memes')
   return await memes
     .updateOne({ name }, { $pull: { tags: tag } })
