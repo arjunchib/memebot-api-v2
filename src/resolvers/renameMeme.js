38ba75a7b14a82866f1972318d8d0bebe1988a5a
Arjun Chib <arjunpatrickchib@gmail.com>
2019-10-13 20:29:23 -0400

    Fix rename resolver to update commands array using bulk

--- src/resolvers/renameMeme.js
+++ src/resolvers/renameMeme.js
@@ -3,14 +3,22 @@ module.exports = async function({ oldName, newName }, { ip, db }) {
     throw new Error('You are not authenticated to mutation data!')
   }
   const memes = db.collection('memes')
-  return await memes
-    .findOneAndUpdate(
-      { name: oldName },
-      {
-        $set: { name: newName },
-        $pull: { commands: oldName },
-        $addToSet: { commands: newName }
-      }
-    )
-    .then(result => result.value)
+
+  const bulk = memes.initializeOrderedBulkOp()
+
+  bulk.find({ name: oldName }).updateOne({
+    $addToSet: { commands: newName }
+  })
+  bulk.find({ name: oldName }).updateOne({
+    $set: { name: newName },
+    $pull: { commands: oldName }
+  })
+
+  const result = await bulk.execute()
+
+  if (result.writeErrors) {
+    throw result.writeErrors
+  }
+
+  return await memes.findOne({ name: newName }).then(result => result.value)
 }
