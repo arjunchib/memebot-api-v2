86064d8718c91462a35c2d656f4119b9ad49c0da
Matthew Hannah <matthew.r.hannah@gmail.com>
2020-03-06 10:53:55 -0500

    Throw error on removing alias when it is the name

--- src/resolvers/removeAliasFromMeme.js
+++ src/resolvers/removeAliasFromMeme.js
@@ -2,6 +2,13 @@ module.exports = async function({ name, alias }, { ip, db }) {
   if (ip !== process.env.MEMEBOT_IP && process.env.NODE_ENV !== 'development') {
     throw new Error('You are not authenticated to mutation data!')
   }
+
+  if (name == alias) {
+    throw new Error(
+      'You cannot remove the alias for the name itself. Use rename instead'
+    )
+  }
+
   const memes = db.collection('memes')
   return await memes
     .findOneAndUpdate({ name }, { $pull: { commands: alias } })
